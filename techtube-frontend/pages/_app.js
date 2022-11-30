20427147c2e661b707e01f6045324e818019b70c
largis21 <ligiskegjerde@gmail.com>
2022-11-30 18:07:45 +0100

    added favicon

--- techtube-frontend/pages/_app.js
+++ techtube-frontend/pages/_app.js
@@ -2,6 +2,7 @@ import "../styles/globals.css";
 import "../styles/fonts.css";
 
 import App from "next/app";
+import Head from "next/head";
 import { AuthProvider, getUser } from "../lib/context/AuthContext";
 
 import Navbar from "../components/Navbar";
@@ -9,6 +10,9 @@ import Navbar from "../components/Navbar";
 function MyApp({ Component, pageProps, auth }) {
   return (
     <AuthProvider myAuth={auth}>
+      <Head>
+        <link rel="shortcut icon" href="/favicon.svg" />
+      </Head>
       <div className="page-root">
         <div className="background-wrapper">
           <img src="/background.svg" alt="" />
