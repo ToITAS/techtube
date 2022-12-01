801026f1796ec989686ffe2b2d832d2e6dc4aaac
largis21 <ligiskegjerde@gmail.com>
2022-12-01 10:49:53 +0100

    added codeblock

--- techtube-frontend/pages/artikler/[id].js
+++ techtube-frontend/pages/artikler/[id].js
@@ -2,6 +2,15 @@ import { useEffect } from "react";
 import formatUnix from "../../lib/formatUnix";
 import Navbar from "../../components/Navbar";
 import { useAuth } from "../../lib/context/AuthContext";
+import Prism from "prismjs";
+
+export async function getServerSideProps(context) {
+  const response = await fetch(
+    process.env.API_BASE_URL + "/api/artikler/id/" + context.params.id
+  );
+  const article = await response.json();
+  return { props: { article } };
+}
 
 export default function Article({ article }) {
   const { auth } = useAuth();
@@ -10,39 +19,39 @@ export default function Article({ article }) {
     <>
       <Navbar />
       <div className="page-content">
-        <div className="article">
-          <div className="article-head">
-            <h1>{article.tittel}</h1>
-            <h4>
-              Skrevet {formatUnix(article.lagt_til_dato)} av{" "}
-              {article.lagt_til_av.brukernavn}
-            </h4>
+        {article !== {} ? (
+          <div className="article">
+            <div className="article-head">
+              <h1>{article.tittel}</h1>
+              <h4>
+                Skrevet {formatUnix(article.lagt_til_dato)} av{" "}
+                {article.lagt_til_av.brukernavn}
+              </h4>
+            </div>
+            <div className="article-content">
+              {article.moduler
+                ? article.moduler.map((module, index) => {
+                    return (
+                      <div className="article-module" key={index}>
+                        {module.type == "undertittel" ? (
+                          <h2>{module.tekst}</h2>
+                        ) : module.type == "brødtekst" ? (
+                          <p>{module.tekst}</p>
+                        ) : module.type == "kodeblokk" ? (
+                          <p className="codeblock">{module.tekst}</p>
+                        ) : (
+                          ""
+                        )}
+                      </div>
+                    );
+                  })
+                : ""}
+            </div>
           </div>
-          <div className="article-content">
-            {article.moduler
-              ? article.moduler.map((module, index) => (
-                  <div className="article-module" key={index}>
-                    {module.type == "undertittel" ? (
-                      <h2>{module.tekst}</h2>
-                    ) : module.type == "brødtekst" ? (
-                      <p>{module.tekst}</p>
-                    ) : (
-                      ""
-                    )}
-                  </div>
-                ))
-              : ""}
-          </div>
-        </div>
+        ) : (
+          ""
+        )}
       </div>
     </>
   );
-}
-
-export async function getServerSideProps(context) {
-  const response = await fetch(
-    process.env.API_BASE_URL + "/api/artikler/id/" + context.params.id
-  );
-  const article = await response.json();
-  return { props: { article } };
 }
