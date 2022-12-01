22d242fbddd714265ae1ce5377a2cbc771ff6b4e
largis21 <ligiskegjerde@gmail.com>
2022-12-01 14:38:03 +0100

    added image

--- techtube-frontend/pages/artikler/[id].js
+++ techtube-frontend/pages/artikler/[id].js
@@ -38,6 +38,8 @@ export default function Article({ article }) {
                           <p>{module.tekst}</p>
                         ) : module.type == "kodeblokk" ? (
                           <p className="codeblock">{module.tekst}</p>
+                        ) : module.type == "bilde" ? (
+                          <img src={module.tekst} className="article-image" />
                         ) : (
                           ""
                         )}
